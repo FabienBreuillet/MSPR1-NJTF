394dc81444c2ade9de9ab37d29b8c58c7ccba1a7
Fabien <fabien.breuillet@gmail.com>
2021-02-04 11:08:58 +0100

    Continuation d'intégration

--- src/components/UI/Header.vue
+++ src/components/UI/Header.vue
@@ -3,19 +3,24 @@
     <Menu></Menu>
     <Register></Register>
     <Login></Login>
+    <Langage-switch />
 </div>
 </template>
 
 <script>
 import Menu from '../UI/Menu.vue'
+import LangageSwitch from '../UI/LangageSwitch.vue'
 import Login from '../User/Login.vue'
 import Register from '../User/Register.vue'
+
 
 export default {
     components: {
         Menu,
         Register,
-        Login
+        Login,
+        LangageSwitch
     }
 };
 </script>
+       
---
+++ src/components/UI/LangageSwitch.vue
@@ -0,0 +1,8 @@
+<template>
+  <h2> Nation Sounds le concert caritatif </h2>
+</template>
+
+<script>
+export default {
+}
+</script>
